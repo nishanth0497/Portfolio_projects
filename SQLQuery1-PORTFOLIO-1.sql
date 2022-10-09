39c95147eb8c6b509f4ac979a11b7f8abee546ba
nishanth0497 <59798118+nishanth0497@users.noreply.github.com>
2022-10-09 23:52:07 +0530

    Update SQLQuery1-PORTFOLIO-1.sql

--- SQLQuery1-PORTFOLIO-1.sql
+++ SQLQuery1-PORTFOLIO-1.sql
@@ -1,42 +1,42 @@
---select *
---from [project-1-database]..covidvaccination
---order by 3,4
+select *
+from [project-1-database]..covidvaccination
+order by 3,4
 
---select Location, date, total_cases, new_cases, total_deaths, population
---From [project-1-database].. coviddeaths
---order by 1,2
+select Location, date, total_cases, new_cases, total_deaths, population
+From [project-1-database].. coviddeaths
+order by 1,2
 
 --Total cases vs Total deaths : death_percentage in India
---select Location, date, total_cases, total_deaths, (total_deaths/total_cases) * 100 as death_percentage
---From [project-1-database].. coviddeaths
---Where location like 'India'
---order by 1,2
+select Location, date, total_cases, total_deaths, (total_deaths/total_cases) * 100 as death_percentage
+From [project-1-database].. coviddeaths
+Where location like 'India'
+order by 1,2
 
 --Total cases vs Total deaths : covid_contraction_percentage in India
---select Location, date, total_cases, population, (total_cases/population) * 100 as Contraction_percentage
---From [project-1-database].. coviddeaths
---Where location like 'India'
---order by 1,2
+select Location, date, total_cases, population, (total_cases/population) * 100 as Contraction_percentage
+From [project-1-database].. coviddeaths
+Where location like 'India'
+order by 1,2
 
 --Countries with highest infection rates: 
---select Location, Population, MAX(total_cases) as highest_cases, MAX((total_cases/population) * 100) as Contraction_percentage
---From [project-1-database].. coviddeaths
---Group by Location, Population
---order by Contraction_percentage desc
+select Location, Population, MAX(total_cases) as highest_cases, MAX((total_cases/population) * 100) as Contraction_percentage
+From [project-1-database].. coviddeaths
+Group by Location, Population
+order by Contraction_percentage desc
 
 --Countries with highest death count:
---select Location, Population, MAX(cast(total_deaths as int)) as Highest_deaths
---From [project-1-database].. coviddeaths
---where continent is not NULL
---Group by Location, Population
---order by Highest_deaths desc
+select Location, Population, MAX(cast(total_deaths as int)) as Highest_deaths
+From [project-1-database].. coviddeaths
+where continent is not NULL
+Group by Location, Population
+order by Highest_deaths desc
 
 --Continents with highest death count per population
---select continent, MAX(cast(total_deaths as int)) as Highest_deaths
---From [project-1-database]..coviddeaths
---where continent is not NULL
---group by continent
---order by Highest_deaths desc
+select continent, MAX(cast(total_deaths as int)) as Highest_deaths
+From [project-1-database]..coviddeaths
+where continent is not NULL
+group by continent
+order by Highest_deaths desc
 
 --Global Numbers
 select SUM(cast(new_deaths as bigint))as total_deaths, SUM(cast(new_cases as bigint)) as total_cases, SUM(cast(total_cases as bigint))/SUM(cast(total_deaths as bigint)) as death_percentage
